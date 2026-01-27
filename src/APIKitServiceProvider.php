334d2004511b9075f5c10107e0e288814bb438c5
midesweb <miguel@escuela.it>
2026-01-27 19:09:40 +0100

    restore file

---
+++ src/APIKitServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EscuelaIT\APIKit;
+
+use Illuminate\Support\ServiceProvider;
+
+class APIKitServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // ...
+    }
+
+    public function register(): void
+    {
+        // ...
+    }
+}
